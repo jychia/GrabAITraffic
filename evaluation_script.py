a25dbc1cbc21f77a0a98f000cb3733638511cc81
Jia Yuan Chia <jiayuan.chia@besi.com>
2019-06-12 08:58:00 +0800

    Update eval script

--- evaluation_script.py
+++ evaluation_script.py
@@ -14,24 +14,27 @@ from keras.models import load_model
 
 
 
-
-
+# Allow user to enter file path for evaluation dataset
 user_input = input("Enter the file path of your evaluation dataset: ")
-
 assert os.path.exists(user_input), "Invalid file at " + str(user_input)
 f = open(user_input,'r+')
 print("Valid file path entered")
 f.close()
 
+
+# Preprocess the raw data from evaluation dataset
 edited_file_name = preprocessing.preprocessing_rawdata(user_input)
 
 dataset = pd.read_csv(edited_file_name, quoting = 3)
 
+# Transform the processed data into 2D data plots
 testing_img = preprocessing.plot_data_to_img(dataset)
 
-model_name = 'final_model.h5'
+# Load neural network model
+model_name = 'models/final_model.h5'
 seq = load_model(model_name)
 
+# Run predictions for T+1 to T+5
 y_pred = []
 timestep = 96
 X_test = testing_img[testing_img.shape[0]-timestep:testing_img.shape[0],:,:,:]
@@ -43,12 +46,9 @@ for i in range(5):
     X_test = np.delete(X_test, (0), axis=0)
 y_pred = np.array(y_pred)
 
-
-
-
 print("Finish predicting, start post-processing prediction data")
 
-
+# Post process the predictions into data
 final_pred = pd.DataFrame(columns = ['latitude', 'longitude', 'geohash6', 'prediction', 'TPlus'])
 uniquelatitude = dataset['latitude'].unique().tolist()
 uniquelatitude.sort()
